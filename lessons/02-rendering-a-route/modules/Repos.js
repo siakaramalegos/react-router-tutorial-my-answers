2807d9daa8cf4a1b3ecf12a2bf2d5385b14b9731
Sia <siakaramalegos@gmail.com>
2016-07-04 14:37:03 -0500

    URL params

--- lessons/02-rendering-a-route/modules/Repos.js
+++ lessons/02-rendering-a-route/modules/Repos.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Repos = () => {
   return (
-    <div>Repos</div>
+    <div>
+      <h2>Repos</h2>
+
+      <ul>
+        <li>
+          <Link to='repos/reactjs/react-router'>React Router</Link>
+        </li>
+        <li>
+          <Link to='repos/facebook/react'>React</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
